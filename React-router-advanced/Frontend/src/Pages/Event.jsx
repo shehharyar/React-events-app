6b63376f88c15fa7427670d1a00e3d4bb9571b78
shehharyar <sheharyarhassan1456@gmail.com>
2023-05-11 20:39:13 +0500

    used loader-data hook to fetch data and refactored code

--- React-router-advanced/Frontend/src/Pages/Event.jsx
+++ React-router-advanced/Frontend/src/Pages/Event.jsx
@@ -1,31 +1,25 @@
-import { useEffect, useState } from "react";
+
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventPage(){
-    const [isLoading, setIsLoading] = useState(false);
-    const [fetchedEvents, setFetchedEvents] = useState();
-    const [error, setError] = useState();
-  
-    useEffect(() => {
-      async function fetchEvents() {
-        setIsLoading(true);
-        
-        setIsLoading(false);
-      }
-  
-      fetchEvents();
-    }, []);
+const events= useLoaderData();
+
     return (
       <>
-        <div style={{ textAlign: 'center' }}>
-          {isLoading && <p>Loading...</p>}
-          {error && <p>{error}</p>}
-        </div>
-        {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={events} />
       </>
     );
   }
-    
-
-
+  
 export default EventPage;
+export async function loader(){
+  const response = await fetch('http://localhost:8080/events');
+
+        if (!response.ok) {
+          //....
+        } else {
+          const resData = await response.json();
+          return resData.events;  
+        }
+}
