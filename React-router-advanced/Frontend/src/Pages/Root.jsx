27b0870f2be5abc9fb759b0a1287f0120d9b4de3
shehharyar <sheharyarhassan1456@gmail.com>
2023-05-12 22:44:55 +0500

    added navigation for loading logic

--- React-router-advanced/Frontend/src/Pages/Root.jsx
+++ React-router-advanced/Frontend/src/Pages/Root.jsx
@@ -1,10 +1,12 @@
 import MainNavigation from "../components/MainNavigation";
-import { Outlet} from "react-router-dom"
+import { Outlet, useNavigation} from "react-router-dom"
 export default function RootLayOut(){
+   const navigation= useNavigation();
     return(
         <>
         <MainNavigation/>
         <main>
+            {navigation.state  ==="loading" && <p>Loading...</p>}
             <Outlet/>
         </main>
         </>
