8b6e6bb512a2bc559f00164fcb58291c681c9758
shehharyar <sheharyarhassan1456@gmail.com>
2023-05-14 23:21:02 +0500

    added json utitlity function

--- React-router-advanced/Frontend/src/Pages/Event.jsx
+++ React-router-advanced/Frontend/src/Pages/Event.jsx
@@ -1,9 +1,14 @@
 
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventPage(){
 const data= useLoaderData();
+
+if(data.isError){
+  return <p>{data.message}</p>
+}
+
 const events= data.events;
     return (
       <>
@@ -14,11 +19,18 @@ const events= data.events;
   
 export default EventPage;
 export async function loader(){
-  const response = await fetch('http://localhost:8080/events');
+  const response = await fetch('http://localhost:8080/events/something');
 
         if (!response.ok) {
-          //....
+          // throw new Response(JSON.stringify({message: "Coul not fetch events."}),
+          // {
+          //   status: 500
+          // }
+          // )
+          return json({message: "Coul not fetch events."},{ status: 500 });
+
         } else {
           return response;
         }
+        
 }
